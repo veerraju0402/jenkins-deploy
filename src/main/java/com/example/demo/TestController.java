60fff02cd2fd6c61678e8f20afded9531ec76c73
veerr <veerr@V_RAJ>
2024-04-24 13:55:28 +0530

    refresh

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
 public class TestController {
 
+	 @GetMapping
+	    public void wel(HttpServletResponse response) throws IOException {
+	         response.sendRedirect("swagger-ui.html");
+	    }
+
+	 
     @GetMapping("/msg")
     public String welcome() {
         return "welcome message";
     }
 
-    @PostMapping("msg")
+    @PostMapping("/msg")
     public String welcome(@RequestParam String str) {
         return "welcome message:" + str;
     }
